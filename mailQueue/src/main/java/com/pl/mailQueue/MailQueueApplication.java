40bc67892172a478aed1515aba6a3ffaf9bf5786
PMushy <44428572+PMushy@users.noreply.github.com>
2019-07-15 13:38:19 +0200

    Add files via upload

---
+++ mailQueue/src/main/java/com/pl/mailQueue/MailQueueApplication.java
@@ -0,0 +1,13 @@
+package com.pl.mailQueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailQueueApplication.class, args);
+	}
+
+}
---
+++ mailQueue/src/test/java/com/pl/mailQueue/MailQueueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pl.mailQueue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailQueueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
